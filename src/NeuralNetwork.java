184e7b22d49e7893221b4f3e8ee335423401dc2c
Eldar Kusdavletov <eldar.kusd@gmail.com>
2023-11-17 17:25:00 +0900

    changed some logic in back propagation

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -13,15 +13,15 @@ public class NeuralNetwork {
     public NeuralNetwork(double learningRate_, int... sizes) {
         learningRate = learningRate_;
         layers = new Layer[sizes.length];
-        for (int k = 0; k < sizes.length; k++) {
+        for (int k = 0; k < sizes.length; k ++) {
             int nextSize = 0;
             if (k < sizes.length - 1) nextSize = sizes[k + 1];
 
             layers[k] = new Layer(sizes[k], nextSize);
 
-            for (int i = 0; i < sizes[k]; i++) {
+            for (int i = 0; i < sizes[k]; i ++) {
                 layers[k].biases[i] = Math.random() * 2.0 - 1.0;
-                for (int j = 0; j < nextSize; j++) {
+                for (int j = 0; j < nextSize; j ++) {
                     layers[k].weights[i][j] = Math.random() * 2.0 - 1.0;
                 }
             }
@@ -30,51 +30,94 @@ public class NeuralNetwork {
 
     public double[] forwardPropagation(double[] inputs) {
         System.arraycopy(inputs, 0, layers[0].neurons, 0, inputs.length);
-        for (int k = 1; k < layers.length; k ++) {
-            for (int i = 0; i < layers[k].size; i ++) {
+        for (int k = 1; k < layers.length; k++) {
+            for (int i = 0; i < layers[k].size; i++) {
                 layers[k].neurons[i] = layers[k].biases[i];
-                for (int j = 0; j < layers[k - 1].size; j ++) {
+                for (int j = 0; j < layers[k - 1].size; j++) {
                     layers[k].neurons[i] += layers[k - 1].neurons[j] * layers[k - 1].weights[j][i];
                 }
-                if (k != layers.length - 1)
-                    layers[k].neurons[i] = activation.apply(layers[k].neurons[i]);
-                else
-                    layers[k].neurons[i] = output_activation.apply(layers[k].neurons[i]);
+
+                layers[k].neurons[i] = activation.apply(layers[k].neurons[i]);
             }
         }
         return layers[layers.length - 1].neurons;
     }
 
+    /*public void backwardPropagation(double[] targets) {
+        Layer outputLayer = layers[layers.length - 1];
+
+        double[] errors = new double[outputLayer.size];
+        double gradients[] = new double[outputLayer.size];
+
+        for (int i = 0; i < outputLayer.size; i++) {
+            gradients[i] = (outputLayer.neurons[i] - targets[i]) * d_activation.apply(outputLayer.neurons[i]);
+            errors[i] = (outputLayer.neurons[i] - targets[i]);
+            // gradients[i] = errors[i] * d_activation.apply(outputLayer.neurons[i]);
+        }
+
+        for (int k = layers.length - 2; k >= 0; k--) {
+            Layer l1 = layers[k + 1], l = layers[k];
+            double[] errorsNext = new double[l.size];
+            double newGradients[] = new double[l.size];
+
+            for (int i = 0; i < l.size; i++) {
+                errorsNext[i] = 0;
+                newGradients[i] = 0;
+                for (int j = 0; j < l1.size; j++) {
+                    errorsNext[i] += l.weights[i][j] * errors[j];
+                    newGradients[i] += l.weights[i][j] * gradients[j] * (d_activation.apply(layers[k].neurons[i]) / d_activation.apply(layers[k + 1].neurons[j]));
+                }
+            }
+
+            // gradients[i] = errors[i] * d_activation.apply(layers[k + 1].neurons[i]);
+
+            for (int i = 0; i < l1.size; i++) {
+                for (int j = 0; j < l.size; j++) {
+                    // l.weights[j][i] += learningRate * l.neurons[j] * errors[i] * d_activation.apply(layers[k + 1].neurons[i]);
+                    l.weights[j][i] += learningRate * l.neurons[j] * gradients[i]; // errors[i] * d_activation.apply(outputLayer.neurons[i]);
+                }
+            }
+
+            for (int i = 0; i < l1.size; i++) {
+                // l1.biases[i] += learningRate * errors[i] * d_activation.apply(layers[k + 1].neurons[i]);
+                l1.biases[i] += learningRate * gradients[i]; // errors[i] * d_activation.apply(outputLayer.neurons[i]);
+            }
+
+            gradients = newGradients;
+            errors = errorsNext;
+        }
+    }*/
+
     public void backwardPropagation(double[] targets) {
         Layer outputLayer = layers[layers.length - 1];
-        double gradients[] = new double[outputLayer.size];
-        for (int i = 0; i < outputLayer.size; i ++)
-            gradients[i] = (outputLayer.neurons[i] - targets[i]) * d_activation.apply(outputLayer.neurons[i]);
+        double errors[] = new double[outputLayer.size];
 
-        for (int k = layers.length - 2; k >= 0; k --) {
-            Layer l1 = layers[k + 1], l = layers[k];
+        for (int i = 0; i < outputLayer.size; i++)
+            errors[i] = outputLayer.neurons[i] - targets[i];
 
-            double newGradients[] = new double[l.size];
-            for (int i = 0; i < l.size; i ++) {
-                newGradients[i] = 0;
-                for (int j = 0; j < l1.size; j ++) {
-                    newGradients[i] += gradients[j] * l.weights[i][j];
+        for (int k = layers.length - 2; k >= 0; k--) {
+            Layer l = layers[k], l1 = layers[k + 1];
+
+            double[] newErrors = new double[l.size];
+            for (int i = 0; i < l.size; i++) {
+                newErrors[i] = 0;
+                for (int j = 0; j < l1.size; j++) {
+                    newErrors[i] += l.weights[i][j] * errors[j];
+                }
+                newErrors[i] *= l.neurons[i] * (1 - l.neurons[i]);
+            }
+
+            for (int i = 0; i < l.size; i++) {
+                for (int j = 0; j < l1.size; j++) {
+                    l.weights[i][j] -= learningRate * errors[j] * l.neurons[i];
                 }
             }
 
-            double[][] newWeights = new double[l.size][l1.size];
-            for (int i = 0; i < l.size; i ++) {
-                for (int j = 0; j < l1.size; j ++) {
-                    l.weights[i][j] -= learningRate * l.neurons[i] * gradients[j];
-                }
+            for (int j = 0; j < l1.size; j++) {
+                l1.biases[j] -= learningRate * errors[j];
             }
 
-            for (int j = 0; j < l1.size; j ++) {
-                l1.biases[j] -= learningRate * gradients[j];
-            }
-
-            l.weights = newWeights;
-            gradients = newGradients;
+            errors = newErrors;
         }
     }
 
